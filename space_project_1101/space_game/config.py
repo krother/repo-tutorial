3a2cd811d799accb80b2a7d95bef10c03f4870bb
Kristian Rother <kristian.rother@posteo.de>
2026-04-17 11:01:37 +0200

    uff repo with dict is working

---
+++ space_project_1101/space_game/config.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+BASE_PATH = Path(__file__).parent
+load_dotenv(dotenv_path=BASE_PATH.parent / ".env", override=True, verbose=True)
+
+DATA_PATH = BASE_PATH.parent / "data"
+
+LANG = os.getenv("LANGUAGE")
